87e60aab297c188302fb139116e1b697c54c615b
nabinsaru <nabinsaru@bajratechnologies.com>
2023-12-08 17:10:20 +0545

    feat: add functionality to track and manipulate the logger

---
+++ src/helpers/file-system.helper.ts
@@ -0,0 +1,59 @@
+import { writeFileSync, existsSync, mkdirSync } from "fs";
+import * as path from "path";
+
+function findProjectRoot(currentDir: string, marker = "package.json"): string {
+  const rootPath = path.parse(currentDir).root;
+
+  while (currentDir !== rootPath) {
+    const markerPath = path.join(currentDir, marker);
+
+    if (existsSync(markerPath)) {
+      return currentDir;
+    }
+
+    currentDir = path.dirname(currentDir);
+  }
+
+  throw new Error(`Marker "${marker}" not found in the directory tree.`);
+}
+
+const rootPath = findProjectRoot(__dirname);
+
+function createFile(filePath: string) {
+  // Normalize the path to handle both Unix-like and Windows separators
+  const normalizedPath = path.normalize(filePath);
+
+  const pathParts = normalizedPath.split(path.sep);
+  const fileName = pathParts.pop() || "j-logger.log";
+
+  // Create the directory structure
+  const fileDir = path.resolve("", ...pathParts);
+
+  if (!existsSync(fileDir)) {
+    mkdirSync(fileDir);
+  }
+
+  const filePathWithFile = path.join(fileDir, fileName);
+  writeFileSync(filePathWithFile, "");
+
+  console.log(`[INFO]: File "${filePathWithFile}" has been created.`);
+}
+
+export function writeLog(filePath: string, msg: string): boolean {
+  try {
+    const fullFilePath = path.join(rootPath, filePath);
+
+    if (!existsSync(fullFilePath)) {
+      createFile(fullFilePath);
+    }
+
+    writeFileSync(fullFilePath, `${msg}\n`, { flag: "a" });
+
+    return true;
+  } catch (error: any) {
+    console.log(error);
+    return false;
+  }
+}
+
+export const RootPath = rootPath;
