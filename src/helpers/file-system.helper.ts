e03de5d3ca007e0a4b3eb0c2068e9321aed1b02c
nabinsaru <nabinsaru@bajratechnologies.com>
2023-12-13 14:54:05 +0545

    refactor: update the logic to traverse root path based on the git file or the node modules

--- src/helpers/file-system.helper.ts
+++ src/helpers/file-system.helper.ts
@@ -1,19 +1,37 @@
 import { writeFileSync, existsSync, mkdirSync, statSync } from "fs";
+import { spawnSync } from "child_process";
 import * as path from "path";
 
-function findProjectRoot(currentDir: string, marker = "package.json"): string {
-  const rootPath = path.parse(currentDir).root;
+function findProjectRoot(currentDir: string, marker = "node_modules"): string {
+  const { error, stdout } = spawnSync(`git worktree list --porcelain`, {
+    encoding: "utf8",
+    shell: true,
+  });
 
-  while (currentDir !== rootPath) {
-    const markerPath = path.join(currentDir, marker);
+  if (stdout) {
+    // find the root path based on the git structure
+    const paths = stdout
+    .split("\n")
+    .map((line) => {
+      const [key, value] = line.split(/\s+/) || [];
+      return key === "worktree" ? value : "";
+    })
+    .filter(Boolean);
+    return paths[0];
+  } else {
+    // find the root path based on the node structure
+    const rootPath = path.parse(currentDir).root;
 
-    if (existsSync(markerPath)) {
-      return currentDir;
+    while (currentDir !== rootPath) {
+      const markerPath = path.join(currentDir, marker);
+
+      if (existsSync(markerPath)) {
+        return currentDir;
+      }
+
+      currentDir = path.dirname(currentDir);
     }
-
-    currentDir = path.dirname(currentDir);
   }
-
   throw new Error(`Marker "${marker}" not found in the directory tree.`);
 }
 
@@ -47,7 +65,7 @@ function createFile(filePath: string, isDir: boolean = false) {
 
 export function writeLog(filePath: string, msg: string): boolean {
   try {
-    let fullFilePath = path.join(rootPath, filePath);
+    let fullFilePath = path.join(rootPath, filePath || "j-logger.log");
 
     if (!existsSync(fullFilePath)) {
       createFile(fullFilePath);
